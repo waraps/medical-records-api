f9d08a7be4f01987f8e6c07426b3051c759cdb50
Manuel Meneses <manuelmenesesg@gmail.com>
2023-01-05 19:26:25 +0000

    chore: update seeders

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,28 +8,20 @@ const prisma = new PrismaClient()
 async function main() {
     await prisma.rol.createMany({
         data: [
-            {
-                name: 'administrator',
-            },
-            {
-                name: 'doctor',
-            },
-            {
-                name: 'receptionist',
-            },
+            { name: 'administrator' }, { name: 'doctor' }, { name: 'receptionist' },
         ],
     });
 
-    const hash = await argon.hash(config.get('ADMIN_PASSWORD'));
+    const hash = await argon.hash(config.get('DEFAULT_PASSWORD'));
     await prisma.user.createMany({
       data: [
           {
               email: "galenosclinica.vet@gmail.com",
               password: hash,
               first_name: "Galenos",
-              last_name: "Clinica Veterinaria",
+              last_name: "Clinica Vet",
               dni: "00000000",
-              avatar: "https://gravatar.com/avatar/0f37b1761eca504fd2d72045b2706330?s=400&d=robohash&r=x",
+              avatar: "https://i.ibb.co/C2Vw01w/galenos.png",
               rol_id: 1,
           },
       ],
@@ -37,12 +29,16 @@ async function main() {
 
     await prisma.petSex.createMany({
         data: [
-            {
-                name: 'female',
-            },
-            {
-                name: 'male',
-            },
+            { name: 'female', }, { name: 'male' },
+        ],
+    });
+
+    await prisma.test.createMany({
+        data: [
+            { name: 'HEMATOLOGÍA'}, { name: 'QUÍMICA' }, { name: 'CLP' },
+            { name: '4DX' }, { name: 'T4' }, { name: 'RX' }, { name: 'ELECTRO' },
+            { name: 'US' }, { name: 'ECOCARDIO' }, { name: 'SEROLOGIA PARVOVIRUS' },
+            { name: 'FELV/FIV' }, { name: 'DENMATOF' }, { name: 'RASPADO' },
         ],
     });
 }
